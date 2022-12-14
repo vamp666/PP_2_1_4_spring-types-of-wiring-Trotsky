1eb13e7fe15fc74e2486901854ab8786f6224caa
Vladislav Trotsky <87899580+vamp666@users.noreply.github.com>
2022-12-14 08:51:57 +0400

    Add files via upload

---
+++ src/main/java/koschei/config/AppConfig.java
@@ -0,0 +1,22 @@
+package koschei.config;
+
+import koschei.models.Island2;
+import koschei.models.Rabbit4;
+import koschei.models.Wood3;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "koschei")
+public class AppConfig {
+
+    @Bean
+    public static Island2 getIsland(@Qualifier("wood3") Wood3 wood) {
+        return new Island2(wood);
+    }
+
+    @Bean
+    public static Wood3 getWood(Rabbit4 rabbit) { return new Wood3(rabbit); }
+}
